249c8b8c2b991f89953aa960aa5dc64acf087b26
YESSER TECH <aidenyassin115@gmail.com>
2024-10-16 14:22:09 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "🔎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/botdep24/Immortal_dragon';
-  const img = 'https://files.catbox.moe/ok01oz.jpg';
+  const githubRepo = 'https://api.github.com/repos/Yassin994/Zombie';
+  const img = https://files.catbox.moe/won03k.jpeg';
 
   try {
     const response = await fetch(githubRepo);
@@ -22,19 +22,19 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "🔎", nomFichier: __
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
       const gitdata = `*Hey Pal? You love the bot Right?*\n  
-      *Here is all you need to Know*
+      *zombie md*
 ╭─────────────────
-││ *𝐒𝐞𝐬𝐬𝐢𝐨𝐧* https://web-vvvf.onrender.com/
+││ *𝐒𝐞𝐬𝐬𝐢𝐨𝐧* https://yesser.onrender.com/
 ││ *𝐑𝐞𝐩𝐨:* ${data.html_url}
 ││ *𝐒𝐭𝐚𝐫𝐬:* ${repoInfo.stars}
 ││ *𝐅𝐨𝐫𝐤𝐬:* ${repoInfo.forks}
 ││ *𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐃𝐚𝐭𝐞:* ${releaseDate}
 ││ *𝐔𝐩𝐝𝐚𝐭𝐞𝐝:* ${repoInfo.lastUpdate}
-││ *𝐎𝐰𝐧𝐞𝐫:* Sir Antony 
-││ *𝐂𝐡𝐚𝐧𝐧𝐞𝐥:* https://whatsapp.com/channel/0029VakcYbvEawdlEwGTtt39
-││ *𝐘𝐨𝐮𝐭𝐮𝐛𝐞:* https://youtube.com/@wemacomic
+││ *𝐎𝐰𝐧𝐞𝐫:* yessertech
+││ *𝐂𝐡𝐚𝐧𝐧𝐞𝐥:* https://whatsapp.com/channel/0029VakA1mu35fM18opH1s30
+││ *𝐘𝐨𝐮𝐭𝐮𝐛𝐞:* https://www.youtube.com/@Yesserboy92
 ╰─────────────────
-          *Immortal Dragon*`;
+          *role by yessertech*`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
     } else {
