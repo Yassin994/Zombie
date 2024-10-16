7e8742f7bcaf14eafb6c6ee3e759808c309ece1f
YESSER TECH <aidenyassin115@gmail.com>
2024-10-16 13:17:08 +0300

    Update Reboot.js

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("Immortal Dragon Restarting ⏳");
+    repondre("zombie Restarting ⏳");
 
   exec("pm2 restart all");
   
