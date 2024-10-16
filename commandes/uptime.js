c2229fe2e366aa6eabb0fba37346a14eb69cba6f
YESSER TECH <aidenyassin115@gmail.com>
2024-10-16 14:02:02 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -57,7 +57,7 @@ let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURICompo
 
 let res = await getBuffer(linkkk);
 
-await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 *Bugatti*'}, { quoted: ms });
+await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 *yessertech*'}, { quoted: ms });
 
 
 }
